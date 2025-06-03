8d8c2a5be2957e68fa75ef4a66d141d4362aa0cf
loewey <grandmasterlouis@gmail.com@>
2025-06-03 14:27:04 +0100

    changed the colours of the shop, linked some of the routes together, and changed the shop schema to use a different collection

--- models/ShopCropModel.js
+++ models/ShopCropModel.js
@@ -3,6 +3,6 @@ const mongoose = require('mongoose');
 const ShopCropSchema = new mongoose.Schema({
     name: {type: String, required: true},
     quantity: {type: Number, default: 1, min: 1}
-});
+}, {collection: 'shop'});
 
-module.exports = mongoose.model('ShopCrop', ShopCropSchema, 'crops')
+module.exports = mongoose.model('ShopCrop', ShopCropSchema, 'shop');

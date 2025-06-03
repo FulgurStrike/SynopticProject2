c7a89eceaeac7b4f6beb2b685ca73a8264d4140d
loewey <grandmasterlouis@gmail.com@>
2025-06-03 11:53:27 +0100

    I added quantity changing for admin key

--- routes/ShopRoutes.js
+++ routes/ShopRoutes.js
@@ -8,4 +8,5 @@ router.get('/cropShop', shopController.renderCropShopPage);
 router.get('/api/shopData', shopController.getShopData);
 router.post('/api/addItem', shopController.addItem);
 router.post('/api/removeItem', shopController.removeItem);
+router.post('/api/updateQuantity', shopController.updateQuantity);
 module.exports = router;

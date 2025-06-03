746a499366a0e23c287fda2199e12ff84f7476e9
loewey <grandmasterlouis@gmail.com@>
2025-06-03 13:45:39 +0100

    started working on the trading schema

--- models/ShopCropModel.js
+++ models/ShopCropModel.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const ShopCropSchema = new mongoose.Schema({
     name: {type: String, required: true},
-    quantity: {type: Number, default: 0, min: 0}
+    quantity: {type: Number, default: 1, min: 1}
 });
 
 module.exports = mongoose.model('ShopCrop', ShopCropSchema, 'crops')
--- routes/CropPageRoutes.js
+++ routes/CropPageRoutes.js
@@ -2,6 +2,6 @@ const express = require('express');
 const router = express.Router();
 const CropDatabaseController = require('../controllers/CropDatabaseController');
 
-router.get("/", CropDatabaseController.renderCropPage);
+router.get("/cropPage", CropDatabaseController.renderCropPage);
 
 module.exports = router;

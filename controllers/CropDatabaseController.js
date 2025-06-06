ba13f0071f7d1c1869160edae4d6fe6c5bafb390
Aiden Ayers <azurefalctis@proton.me>
2025-06-06 13:36:46 +0100

    Started sort feature

--- controllers/CropDatabaseController.js
+++ controllers/CropDatabaseController.js
@@ -5,11 +5,34 @@ async function getCrops() {
   return crops;
 }
 
-exports.renderCropPage = async (req, res) => {
+async function generateCropPageContent() {
+  const crops = await getCrops();
+
   const CropPageContent = {
     title: "Crop Page",
-    crops: await getCrops()
+    sortOptions: ["Name", "Cycle", "Drought Tolerance", "Watering", "Soil", "Growth Rate", "Fruits In"],
+    filterOptions: ["Annual", "Summer", "Spring", "Autumn", "Winter"],
+    crops
   };
 
-  res.render("cropPage", CropPageContent);
+  return CropPageContent;
+}
+
+
+
+exports.sortBy = async (req, res) => {
+  const sortParam = req.body.sort;
+
+  const sortedCrops = Crop.find({}).sort({ [sortParam.toLowerCase()]: 1 });
+
+  const content = await generateCropPageContent();
+  content.crops = sortedCrops;
+
+  res.render("cropPage", content);
+}
+
+exports.renderCropPage = async (req, res) => {
+  const cropPageContent = await generateCropPageContent();
+
+  res.render("cropPage", cropPageContent);
 }
--- routes/CropPageRoutes.js
+++ routes/CropPageRoutes.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const CropDatabaseController = require('../controllers/CropDatabaseController');
 
 router.get("/cropPage", CropDatabaseController.renderCropPage);
+router.post("/cropPage/sort", CropDatabaseController.sortBy);
 
 module.exports = router;
